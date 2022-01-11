e1be93042b2d27577a3dfdd154aedead4527b31f
Will Weaver <monkey@buildingbananas.com>
2022-01-11 16:23:15 -0500

    Dynamically get the number of screens available

--- files/xbar/001-monitor-manager.1m.sh
+++ files/xbar/001-monitor-manager.1m.sh
@@ -9,10 +9,6 @@
 # <xbar.dependencies>bash</xbar.dependencies>
 # <xbar.abouturl>https://simonpeier.github.io/bitbar-dadjokes-plugin/</xbar.abouturl>
 
-# Confirm that mirror is installed and accessible
-# Confirm that ddcctl/m1ddc is install and accessible
-# Switch to ddcctl once it works for m1 chips until then use m1ddc
-
 function exists() {
   app=$1
   command -v $app 1>/dev/null || { echo "$app is required but not installed."; exit;}
@@ -22,18 +18,31 @@ echo "Monitor Manager"
 echo "---"
 
 exists mirror
-exists m1ddc
-
 echo "Mirror Screens| shell=mirror terminal=false"
 echo "---"
-echo "Top Screen"
-echo "-- HDMI 1| shell=m1ddc param1=display param2=2 param3=set param4=input param5=17 refresh=true"
-echo "-- HDMI 2| shell=m1ddc param1=display param2=2 param3=set param4=input param5=17 refresh=true"
-echo "-- Display Port| shell=m1ddc param1=display param2=2 param3=set param4=input param5=15  refresh=true"
-echo "-- USB C| shell=m1ddc param1=display param2=2 param3=set param4=input param5=27 refresh=true"
-echo "---"
-echo "Bottom Screen"
-echo "-- HDMI 1| shell=m1ddc param1=display param2=2 param3=set param4=input param5=17 refresh=true"
-echo "-- HDMI 2| shell=m1ddc param1=display param2=2 param3=set param4=input param5=17 refresh=true"
-echo "-- Display Port| shell=m1ddc param1=display param2=2 param3=set param4=input param5=15  refresh=true"
-echo "-- USB C| shell=m1ddc param1=display param2=2 param3=set param4=input param5=27 refresh=true"
+# Switch to ddcctl once it works for m1 chips until then use m1ddc
+exists m1ddc
+
+screen=1
+screens=$(m1ddc display list | wc -l)
+screens=2
+
+# Can I remap the order of the screens based on their positions?
+while [ "$screen" -le "$screens" ]; do
+  # Can I create friendly names for the monitor and have them match?
+  # This is a cludge that I'm not even sure will persist. I know that there is info available via ddc
+  if [ $screen -eq 1 ]; then
+    echo "Bottom Screen"
+  elif [ $screen -eq 2 ]; then
+    echo "Top Screen"
+  else
+    echo "Screen $screen"
+  fi
+
+  # Is it possible to find out what input sources are available from the monitor
+  echo "-- HDMI 1| shell=m1ddc param1=display param2=$screen param3=set param4=input param5=17 refresh=true"
+  echo "-- HDMI 2| shell=m1ddc param1=display param2=$screen param3=set param4=input param5=17 refresh=true"
+  echo "-- Display Port| shell=m1ddc param1=display param2=$screen param3=set param4=input param5=15  refresh=true"
+  echo "-- USB C| shell=m1ddc param1=display param2=$screen param3=set param4=input param5=27 refresh=true"
+  ((screen=screen+1))
+done
